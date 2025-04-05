04bd2ba033086f554344131d1ddc9a2206d44b34
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-04-05 14:45:40 +0900

    :test_tube: test: comment 관련 초기 테스트 추가

--- src/main/java/com/example/harumeonglog/domain/comment/controller/port/CommentService.java
+++ src/main/java/com/example/harumeonglog/domain/comment/controller/port/CommentService.java
@@ -14,4 +14,6 @@ public interface CommentService {
     Comment createComment(CommentRequest.CommentCreateRequest commentCreateRequest);
 
     void deleteComment(Long commentId);
+
+    void likeComment(Long commentId);
 }
