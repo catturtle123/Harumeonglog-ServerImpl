05ca07d0f161ea7d10166c814ad5bb46092bc3a8
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-04-09 14:03:36 +0900

    :hammer: refactor: notice 변경

--- src/main/java/com/example/harumeonglog/domain/member/service/NoticeServiceImpl.java
+++ src/main/java/com/example/harumeonglog/domain/member/service/NoticeServiceImpl.java
@@ -2,10 +2,12 @@ package com.example.harumeonglog.domain.member.service;
 
 import com.example.harumeonglog.domain.member.controller.port.NoticeService;
 import com.example.harumeonglog.domain.member.domain.Notice;
+import lombok.Builder;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
+@Builder
 public class NoticeServiceImpl implements NoticeService {
     @Override
     public Slice<Notice> getNotices(Integer size, Long cursor) {
