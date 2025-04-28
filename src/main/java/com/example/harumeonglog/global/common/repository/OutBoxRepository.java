b5651ac525b0c79b2dc1bc40f847bea687322125
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-04-28 16:14:02 +0900

    :bug: fix: 성공 후 다시 시도 시 막는 로직 추가

--- src/main/java/com/example/harumeonglog/global/common/repository/OutBoxRepository.java
+++ src/main/java/com/example/harumeonglog/global/common/repository/OutBoxRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface OutBoxRepository extends JpaRepository<OutBox, Long> {
 
-    @Query("select o from OutBox o where o.eventType = :eventType and o.retryCount < 3 order by o.createdAt asc")
+    @Query("select o from OutBox o where o.eventType = :eventType and o.processed = false and o.retryCount < 3 order by o.createdAt asc")
     List<OutBox> findTopOutBox(Integer retryCount, EventType eventType, Pageable pageable);
 }
