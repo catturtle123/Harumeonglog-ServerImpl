7280e618ca7708bd8155da1b26146a6b89acced7
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-04-30 14:55:57 +0900

    :memo: docs: 헬스 체킹 swagger

--- src/main/java/com/example/harumeonglog/global/common/HealthCheckController.java
+++ src/main/java/com/example/harumeonglog/global/common/HealthCheckController.java
@@ -1,12 +1,21 @@
 package com.example.harumeonglog.global.common;
 
 import com.example.harumeonglog.global.common.response.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health Check", description = "health check 관련 API 입니다.")
 public class HealthCheckController {
 
+    @Operation(summary = "헬스 체킹 API by 김준환", description = "서버 상태 확인하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "성공입니다.")
+    })
     @GetMapping("/health")
     public CustomResponse<String> healthCheck() {
         return CustomResponse.ok("I am healthy");
