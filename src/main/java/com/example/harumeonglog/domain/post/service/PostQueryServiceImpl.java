28d3e6826a78184854b6104be50e175ffd43644e
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-05-14 14:29:13 +0900

    :rocket: chore: 이미지 url 조회 넣기

--- src/main/java/com/example/harumeonglog/domain/post/service/PostQueryServiceImpl.java
+++ src/main/java/com/example/harumeonglog/domain/post/service/PostQueryServiceImpl.java
@@ -7,11 +7,11 @@ import com.example.harumeonglog.domain.post.controller.enums.PostRequestCategory
 import com.example.harumeonglog.domain.post.converter.PostConverter;
 import com.example.harumeonglog.domain.post.dto.response.PostResponse;
 import com.example.harumeonglog.domain.post.entity.Post;
-import com.example.harumeonglog.domain.post.entity.PostImage;
 import com.example.harumeonglog.domain.post.entity.enums.PostCategory;
 import com.example.harumeonglog.domain.post.repository.PostRepository;
 import com.example.harumeonglog.global.error.code.PostErrorCode;
 import com.example.harumeonglog.global.error.exception.PostException;
+import com.example.harumeonglog.global.util.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
@@ -26,6 +26,7 @@ import java.util.List;
 public class PostQueryServiceImpl implements PostQueryService {
 
     private final PostRepository postRepository;
+    private final S3Util s3Util;
 
     @Override
     public PostResponse.PostPreviewListResponse getPosts(Long cursor, Integer size, String search, PostRequestCategory postRequestCategory) {
@@ -47,7 +48,7 @@ public class PostQueryServiceImpl implements PostQueryService {
     public PostResponse.PostDetailResponse getPost(Long postId) {
         Post post = postRepository.findById(postId).orElseThrow(() -> new PostException(PostErrorCode.NOT_FOUND));
         List<String> postImageList = post.getPostImageList().stream()
-                .map(PostImage::getPostImageKeyName)
+                .map((p)->s3Util.getUrlFromKey(p.getPostImageKeyName()))
                 .toList();
         return PostConverter.toPostDetailResponse(post, MemberConverter.toMemberInfoResponse(post.getMember()), postImageList);
     }
@@ -87,7 +88,7 @@ public class PostQueryServiceImpl implements PostQueryService {
         List<PostResponse.PostPreviewResponse> postPreviewResponses = posts.stream()
                 .map(post -> {
                     String postImageKey = post.getPostImageList().isEmpty() ? null :
-                            post.getPostImageList().get(0).getPostImageKeyName();
+                            s3Util.getUrlFromKey(post.getPostImageList().get(0).getPostImageKeyName());
                     return PostConverter.toPostPreviewResponse(post, MemberConverter.toMemberInfoResponse(post.getMember()), postImageKey);
                 })
                 .toList();
